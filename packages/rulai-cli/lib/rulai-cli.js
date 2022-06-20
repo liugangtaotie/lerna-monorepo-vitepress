0e0e856c70523c786e2c5496b1b127f1875d942f
liugang <lgcf1314@gmail.com>
2022-06-20 15:02:56 +0800

    [CHG]:firstv2.2

--- packages/rulai-cli/lib/rulai-cli.js
+++ packages/rulai-cli/lib/rulai-cli.js
@@ -3,7 +3,7 @@
  * @Author: liugang
  * @Date: 2022-06-20 13:34:48
  * @LastEditors: liugang
- * @LastEditTime: 2022-06-20 14:51:40
+ * @LastEditTime: 2022-06-20 15:02:50
  */
 'use strict'
 
@@ -11,6 +11,7 @@ module.exports = rulaiCli
 
 function rulaiCli() {
   // TODO
+  console.info('rulaiCli')
   console.info('rulaiCli')
   console.info('rulaiCli')
   console.info('rulaiCli')

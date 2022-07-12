f86e0c08731db7fc5bc0b16cf6ba35bbe886979e
liugang <lgcf1314@gmail.com>
2022-07-12 09:14:58 +0800

    [CHG]:修改cli

--- packages/rulai-cli/lib/rulai-cli.js
+++ packages/rulai-cli/lib/rulai-cli.js
@@ -3,7 +3,7 @@
  * @Author: liugang
  * @Date: 2022-06-20 13:34:48
  * @LastEditors: liugang
- * @LastEditTime: 2022-06-20 18:48:36
+ * @LastEditTime: 2022-07-12 09:13:56
  */
 'use strict'
 
@@ -11,10 +11,5 @@ module.exports = rulaiCli
 
 function rulaiCli() {
   // TODO
-  console.info('rulaiCli')
-  console.info('rulaiCli')
-  console.info('rulaiCli')
-  console.info('rulaiCli')
-  console.info('rulaiCli')
   console.info('rulaiCli')
 }

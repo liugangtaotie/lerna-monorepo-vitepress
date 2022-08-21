a4b76dfaa2e087f22518048fa178d0425b8755a0
liugang <lgcf1314@gmail.com>
2022-08-21 17:52:15 +0800

    [CHG]:0821firstcommitv1.9

--- packages/Wuxiang/index.vue
+++ packages/Wuxiang/index.vue
@@ -3,16 +3,18 @@
  * @Author: liugang
  * @Date: 2022-08-21 11:21:05
  * @LastEditors: liugang
- * @LastEditTime: 2022-08-21 17:12:14
+ * @LastEditTime: 2022-08-21 17:51:40
 -->
 <template>
-  <div>一切事情，物来则应，过去不留；凡所有相皆是虚妄；光明寂照遍河沙</div>
+  <div>
+    一切事情，物来则应，过去不留；凡所有相皆是虚妄；光明寂照遍河沙；一念不生全体现；
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Wuxiang",
-};
+  name: 'Wuxiang',
+}
 </script>
 
 <style scoped>

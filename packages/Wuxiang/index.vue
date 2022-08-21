59396a7e3a05920079deb2d7227c0c28dde6e442
liugang <lgcf1314@gmail.com>
2022-08-21 16:40:50 +0800

    [CHG]:0821firstcommitv1.5

--- packages/Wuxiang/index.vue
+++ packages/Wuxiang/index.vue
@@ -3,10 +3,10 @@
  * @Author: liugang
  * @Date: 2022-08-21 11:21:05
  * @LastEditors: liugang
- * @LastEditTime: 2022-08-21 16:02:50
+ * @LastEditTime: 2022-08-21 16:40:29
 -->
 <template>
-  <div>一切事情，物来则应，过去不留</div>
+  <div>一切事情，物来则应，过去不留；凡所有相皆是虚妄</div>
 </template>
 
 <script>

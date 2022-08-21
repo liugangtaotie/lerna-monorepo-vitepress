6f5ae36a3ed8e1e2d95f66f12fa7221f66b7cfe9
liugang <lgcf1314@gmail.com>
2022-08-21 13:17:45 +0800

    [CHG]:0821firstcommitv1.2

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -3,7 +3,7 @@
  * @Author: liugang
  * @Date: 2022-08-21 12:51:40
  * @LastEditors: liugang
- * @LastEditTime: 2022-08-21 13:01:06
+ * @LastEditTime: 2022-08-21 13:17:35
  */
 const { getPath } = require('./utils')
 
@@ -12,5 +12,10 @@ module.exports = [
     text: 'Components',
     link: '/components/',
     activeMatch: getPath('^/components/'),
+  },
+  {
+    text: 'Guide',
+    link: '/guide/',
+    activeMatch: getPath('^/guide/'),
   },
 ]

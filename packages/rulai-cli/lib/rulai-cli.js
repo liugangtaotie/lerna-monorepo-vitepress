ca34e2b4df25bd810e85471ac0aed8580b3c2624
liugang <lgcf1314@gmail.com>
2022-10-06 18:38:43 +0800

    feat:删除冗余代码

--- packages/rulai-cli/lib/rulai-cli.js
+++ packages/rulai-cli/lib/rulai-cli.js
@@ -3,14 +3,13 @@
  * @Author: liugang
  * @Date: 2022-06-20 13:34:48
  * @LastEditors: liugang
- * @LastEditTime: 2022-08-27 07:47:49
+ * @LastEditTime: 2022-10-06 18:38:09
  */
 'use strict'
 
 module.exports = rulaiCli
 
 function rulaiCli() {
-  let a = '333'
   // TODO
-  console.info('rulaiCli')
+  console.info('rulai-cli')
 }
